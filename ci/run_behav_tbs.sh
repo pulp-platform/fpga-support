fbf6aa54195bc4398a869886ea2a2ce286b004ca
Andreas Kurth <akurth@iis.ee.ethz.ch>
2019-03-11 18:29:44 +0100

    fixup! Switch from `CfMath` to `cf_math_pkg`

--- ci/run_behav_tbs.sh
+++ ci/run_behav_tbs.sh
@@ -9,7 +9,6 @@ readonly BEHAV_DIR="${ROOT_DIR}/behav"
 cd ${CI_DIR}
 git clone git@iis-git.ee.ethz.ch:pulp-platform/big.pulp.git -b master > /dev/null
 readonly BIGPULP_DIR="${CI_DIR}/big.pulp"
-export CF_MATH_PKG_PATH="${BIGPULP_DIR}/fe/ips/pkg/cfmath"
 
 cd ${BEHAV_DIR}
 make
